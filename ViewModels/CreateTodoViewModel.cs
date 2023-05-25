7440f85bb554adc6e572237ee7c49cd71fbbaf61
Ricardo Francini <ricardo.francini@ambevtech.com.br>
2023-05-25 01:02:13 -0300

    Add-swagger

---
+++ ViewModels/CreateTodoViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace MiniTodo.ViewModels
+{
+    public class CreateTodoViewModel : Notifiable<Notification>
+    {
+        public string Title { get; private set; }
+
+        public Todo MapTo()
+        {
+            var contract = new Contract<Notification>()            
+                .Requires()
+                .IsNotNull(Title, "Informe o título da tarefa")
+                .IsGreaterThan(Title, 5, "O título deve conter mais de 5 Caracteres");
+
+            AddNotifications(contract);
+
+            return new Todo(Guid.NewGuid(), Title, false);
+        }
+    }
+}
